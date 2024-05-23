dc7c6e6bdff88f4ea02659686219bccfe2133754
Reshma <reshma@Rathakrishnans-MacBook-Air.local>
2024-05-22 21:37:34 -0700

    database start

--- models.py
+++ models.py
@@ -19,5 +19,24 @@ def get_time():
 # db.define_table('thing', Field('name'))
 #
 ## always commit your models to avoid problems later
+db.define_table('checklist',
+                Field('sampling_id'),
+                Field('latitude'),
+                Field('longitude'),
+                Field('date', 'date'), #re-check (create function like get_time?)
+                Field('time', 'time'), #re-check
+                Field('email', default=get_user_email),
+                Field('duration', 'integer')
+                )
 
+db.define_table('sightings',
+                Field('sampling_id', 'reference checklist'),
+				Field('name', 'string'),
+				Field('observation_count', 'integer', default=0),               
+                )
+
+db.define_table('species',
+                Field('name', 'reference sightings'),
+                )
 db.commit()
+
